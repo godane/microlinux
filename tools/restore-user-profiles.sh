9db4ae58323822e19c7fd4ff29f77f427a1096a5
Niki Kovacs <info@microlinux.fr>
2015-08-13 09:31:31 +0200

    Script to restore user profiles.

---
+++ tools/restore-user-profiles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -d /etc/skel/.config ]; then
+  continue
+else
+  echo
+  echo ":: Les profils par défaut ne sont pas installés."
+  echo
+  exit 0
+fi
+
+echo
+
+for UTILISATEUR in $(ls /home); do
+  echo ":: Mise à jour du profil de l'utilisateur $UTILISATEUR."
+  rm -rf /home/$UTILISATEUR/.config
+  cp -R /etc/skel/.config /home/$UTILISATEUR/
+  chown -R $UTILISATEUR:users /home/$UTILISATEUR/.config
+done
+
+echo
+exit 1
