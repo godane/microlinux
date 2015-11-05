0081cd82f2886c0805b5e256031ae22c3704b878
Niki Kovacs <info@microlinux.fr>
2015-11-05 15:23:08 +0100

    First set of custom menu entries for MLED 14.2.

---
+++ desktop-14.2-32bit/tools/cleanmenu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# cleanmenu.sh
+# (c) Niki Kovacs, 2014
+
+CWD=$(pwd)
+ENTRIESDIR=$CWD/desktop
+ENTRIES=`ls $ENTRIESDIR` 
+MENUDIRS="  /usr/share/applications \
+	    /usr/share/distcc"
+
+for MENUDIR in $MENUDIRS; do
+	for ENTRY in $ENTRIES; do
+		if [ -r $MENUDIR/$ENTRY ]; then
+			echo ":: Updating $ENTRY."
+			cat $ENTRIESDIR/$ENTRY > $MENUDIR/$ENTRY
+		fi
+	done
+done
+
+if [ -r /usr/share/applications/Thunar-bulk-rename.desktop ]; then
+  if [ $(uname -m) == "x86_64" ]; then
+    sed -i "s/\/usr\/lib\/Thunar/\/usr\/lib64\/Thunar/g" /usr/share/applications/Thunar-bulk-rename.desktop
+  fi
+fi
+
+
+
