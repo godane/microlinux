441247d5cfaf052914cfd388f25ac944be4415f5
Niki Kovacs <info@microlinux.fr>
2016-12-27 09:06:50 +0100

    user-settings-console: Rebuilt.

---
+++ desktop-14.1-source/profile/user-settings-console/doinst.sh
@@ -0,0 +1,32 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+config root/.bashrc.new
+config root/.bash_profile.new
+config root/.bash_logout.new
+config root/.Xresources.new
+config usr/share/vim/vimrc.new
+
+preserve_perms etc/profile.d/lang.sh.new
+preserve_perms etc/rc.d/rc.font.new
